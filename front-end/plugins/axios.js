9d0638d9b380f2e2cc55f8aaf1524a7cec80d1ba
Haewon Han <hhan9243@gmail.com>
2021-08-03 18:00:17 +0900

    working2

--- front-end/plugins/axios.js
+++ front-end/plugins/axios.js
@@ -1,7 +1,3 @@
-export default function({ $axios, redirect }) {
-  $axios.onError(error => {
-    if (error.response.status === 500) {
-      redirect("/sorry");
-    }
-  });
-}
+export default ({ $axios, authorizationToken }) => {
+  $axios.defaults.headers.Authorization = authorizationToken;
+};
